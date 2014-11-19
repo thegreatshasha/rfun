df7afad1cffb0b5ec8e6d3e46fdbb20e78f6217e
Shashwat Kumar <thegreatshasha@gmail.com>
2014-11-19 09:48:52 +0530

    refactored code

--- main.R
+++ main.R
@@ -1,13 +1,27 @@
-file = "~/Downloads/train.csv"
-dat = read.csv(file, header = TRUE)
 library('grid')
+library('foreach')
 
-for(i in 10:100){
-  vectir = as.numeric(dat[i,])[2:785]
-  mat = matrix(vectir, 28, 28, TRUE)/256
-  x <- seq(0, 1, length = nrow(mat))
-  y <- seq(0, 1, length = ncol(mat))
-  print(mat)
+# Read data
+train_file = "~/Downloads/train.csv"
+test_file = "~/Downloads/test.csv"
+train_data = read.csv(train_file, header = TRUE)
+test_data = read.csv(test_file, header = TRUE)
+
+# Useful functions
+euc.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2))
+
+draw_vector <- function(vector) {
+  mat = matrix(vector, 28, 28, TRUE)/256
+  print(label)
   grid.raster(mat)
+}
+
+# Useful constants
+
+# Train Data
+for(i in 1:1000){
+  label = train_data[i,][1]
+  vectir = as.numeric(train_data[i,])[2:785]
+  draw_vector(vectir)
   Sys.sleep(2)
 }
